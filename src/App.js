ca2e84fd428726632219337d58b7bec988bbdba5
Thomas Yoo <yoot@mcmaster.ca>
2021-03-06 16:49:11 -0500

    tutorial complete

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
 import Preferences from './components/Preferences/Preferences';
+import useToken from './components/App/useToken.js';
+
+
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
 
 function App() {
-  
-  const [token, setToken] = useState();
+ 
+  const { token, setToken } = useToken();
 
   
   if(!token) {
