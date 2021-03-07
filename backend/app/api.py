962c02700950b5f3c91588709dc822b04d6a97d4
alex xue <alexxue2001@gmail.com>
2021-03-06 20:07:44 -0500

    password backend

--- backend/app/api.py
+++ backend/app/api.py
@@ -9,7 +9,6 @@ origins = [
     "localhost:3000"
 ]
 
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -18,7 +17,22 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+user_storage = {}
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to your todo list."}
+
+
+@app.put("/auth", tags=["authentication"])
+async def update_todo(id: string, password: string) -> int:
+    """return 2 means welcome back
+        return 1 means new user made
+        return 0 means user exists, password fails
+     """
+    if id in user_storage:
+        if hash(password) == user_storage[id]:
+            return 2
+        return 0
+    user_storage[id] = hash(password)
+    return 1
